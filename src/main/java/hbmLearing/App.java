c892d3c358824346d007f40aa4b7b662d25e8c61
pancham01 <pancham9557@gmail.com>
2023-12-22 20:22:02 +0530

    update

--- src/main/java/hbmLearing/App.java
+++ src/main/java/hbmLearing/App.java
@@ -15,7 +15,6 @@ public class App {
 
 	public static void main(String[] args) {
 
-		// Create typesafe ServiceRegistry object
 		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate1.cfg.xml").build();
 		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
 		SessionFactory sf = meta.getSessionFactoryBuilder().build();
@@ -24,19 +23,19 @@ public class App {
 		System.out.println("Staart...");
 		
 //		Example of HQL to get all the records
-//		Query query1=session.createQuery("from Employee");
-//		List list=query1.list();  
-//		System.out.println(list);
-//		Query q=session.createQuery("select max(name) from Employee");  
-//		List<Integer> list1=q.list();  
-//		System.out.println(list1.get(0));  
+		Query query1=session.createQuery("from Employee");
+		List list=query1.list();  
+		System.out.println(list);
+		Query q=session.createQuery("select max(name) from Employee");  
+		List<Integer> list1=q.list();  
+		System.out.println(list1.get(0));  
 		
 //		HQL to get records with pagination
-//		Query query2=session.createQuery("from Employee");  
-//		query2.setFirstResult(5);  
-//		query2.setMaxResults(2);  
-//		List list2=query2.list();//will return the records from 5 to 10th number  
-//		System.out.println("5 to 10: "+list2);
+		Query query2=session.createQuery("from Employee");  
+		query2.setFirstResult(5);  
+		query2.setMaxResults(5);  
+		List list2=query2.list();//will return the records from 5 to 10th number  
+		System.out.println("5 to 10: "+list2);
 		
 //		HQL update query Using Named Parameters
 //		Query q=session.createQuery("update Employee set name=:n where id=:i");  
@@ -48,9 +47,9 @@ public class App {
 //		Query query=session.createQuery("delete from Employee where id= 20");  
 //		query.executeUpdate();  
 		
-		Query query1=session.createQuery("from Employee");
-		List list=query1.list();  
-		System.out.println(list);
+//		Query query1=session.createQuery("from Employee");
+//		List list=query1.list();  
+//		System.out.println(list);
 
 		tx.commit();
 		session.close();
