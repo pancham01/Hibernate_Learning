c3fcfbc9587454a9e902a22ec12f9ae090532ad7
pancham01 <pancham9557@gmail.com>
2024-11-30 15:09:02 +0530

    update

--- src/main/java/hbmLearing/App.java
+++ src/main/java/hbmLearing/App.java
@@ -11,6 +11,8 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.query.Query;
 
+import hbmLearning.Entity.Employee;
+
 public class App {
 
 	public static void main(String[] args) {
@@ -23,33 +25,33 @@ public class App {
 		System.out.println("Staart...");
 		
 //		Example of HQL to get all the records
-		Query query1=session.createQuery("from Employee");
-		List list=query1.list();  
-		System.out.println(list);
-		Query q=session.createQuery("select max(name) from Employee");  
-		List<Integer> list1=q.list();  
-		System.out.println(list1.get(0));  
+//		Query query1=session.createQuery("from Employee");
+//		List list=query1.list();  
+//		System.out.println(list);
+//		Query q=session.createQuery("select max(name) from Employee",Employee.class);  
+//		List<Integer> list1=q.list();  
+//		System.out.println(list1.get(0));  
 		
 //		HQL to get records with pagination
-		Query query2=session.createQuery("from Employee");  
-		query2.setFirstResult(5);  
-		query2.setMaxResults(5);  
-		List list2=query2.list();//will return the records from 5 to 10th number  
-		System.out.println("5 to 10: "+list2);
+//		Query query2=session.createQuery("from Employee");  
+//		query2.setFirstResult(5);  
+//		query2.setMaxResults(5);  
+//		List list2=query2.list(); 
+//		System.out.println("5 to 10: "+list2);
 		
 //		HQL update query Using Named Parameters
 //		Query q=session.createQuery("update Employee set name=:n where id=:i");  
-//		q.setParameter("n","Udit kumar");  
+//		q.setParameter("n","Vinod");  
 //		q.setParameter("i",1);  
 //		System.out.println("status: "+q.executeUpdate());  
 
 //		HQL delete query
-//		Query query=session.createQuery("delete from Employee where id= 20");  
+//		Query query=session.createQuery("delete from Employee where id= 10");  
 //		query.executeUpdate();  
 		
-//		Query query1=session.createQuery("from Employee");
-//		List list=query1.list();  
-//		System.out.println(list);
+		Query query1=session.createQuery("from Employee");
+		List list=query1.list();  
+		System.out.println(list);
 
 		tx.commit();
 		session.close();
